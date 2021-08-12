bcae8067a6bb4f3441be4cce4793232c4c752eaa
Jasmeet Kaur Sura <200480943@student.georgianc.on.ca>
2021-08-12 06:00:54 -0400

    Add project files.

---
+++ Basics/AuthorizationRequirements/CustomRequireClaim.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basics.AuthorizationRequirements
+{
+    public class CustomRequireClaim : IAuthorizationRequirement
+    {
+          //private object dateOfBirth;
+
+        public CustomRequireClaim(string claimType)
+        {
+            ClaimType = claimType;
+        }
+        //public CustomRequireClaim(object dateOfBirth)
+        //{
+        //    this.dateOfBirth = dateOfBirth;
+        //}
+
+        public string ClaimType { get; }
+    }
+
+    public class CustomRequireClaimHandler : AuthorizationHandler<CustomRequireClaim>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            CustomRequireClaim requirement)
+        {
+            var hasClaim = context.User.Claims.Any(x => x.Type == requirement.ClaimType);
+            if (hasClaim)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        public static AuthorizationPolicyBuilder RequireCustomClaim(
+            this AuthorizationPolicyBuilder builder,
+            string claimType)
+        {
+            builder.AddRequirements(new CustomRequireClaim(claimType));
+            return builder;
+        }
+    }
+}
